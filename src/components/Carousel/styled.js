47d7693d7a369a1e34b0c41054c093bf38699fb7
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-11-28 10:23:08 -0300

    Troca imgs carousel

--- src/components/Carousel/styled.js
+++ src/components/Carousel/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const CarouselStyle = styled.div`
     `
     
-export const Imagens = styled.div`
+    export const Imagens = styled.div`
     img{
         width: 100vw;
         /* height: 300px; */
